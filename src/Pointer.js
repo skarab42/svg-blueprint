216014a236f8931a29f1b1e4ceb07fb95e8ccc02
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-10-23 15:05:00 +0200

    add wheel event

--- src/Pointer.js
+++ src/Pointer.js
@@ -1,11 +1,26 @@
 import Point from "./Point";
 
-function eventFactory({ panning, movement, position }) {
+function eventFactory({ wheelDelta, panning, movement, position }) {
   return {
+    wheelDelta,
     panning,
     movement: movement.clone(),
     position: position.clone()
   };
+}
+
+function updatePosition(pointer, event) {
+  const offsets = pointer.target.getBoundingClientRect();
+  pointer.position = new Point(
+    event.pageX - offsets.left,
+    event.pageY - offsets.top
+  );
+}
+
+function updateMovement(pointer, event) {
+  const position = pointer.position.clone();
+  updatePosition(pointer, event);
+  pointer.movement = pointer.position.sub(position);
 }
 
 /**
@@ -30,22 +45,11 @@ class Pointer {
     /** @type {Point} Position relative to the target. */
     this.position = new Point();
 
-    const updatePosition = event => {
-      const offsets = this.target.getBoundingClientRect();
-      this.position = new Point(
-        event.pageX - offsets.left,
-        event.pageY - offsets.top
-      );
-    };
-
     /** @type {Point} Distance since last move. */
     this.movement = new Point();
 
-    const updateMovement = event => {
-      const position = this.position.clone();
-      updatePosition(event);
-      this.movement = this.position.sub(position);
-    };
+    /** @type {int} Wheel delta. */
+    this.wheelDelta = 0; // -1|+1
 
     // pan listeners
     target.addEventListener("pointerdown", () => {
@@ -54,7 +58,7 @@ class Pointer {
     });
 
     target.addEventListener("pointermove", event => {
-      updateMovement(event);
+      updateMovement(this, event);
       this.emit("move");
       if (!this.panning) {
         return;
@@ -65,6 +69,22 @@ class Pointer {
     target.addEventListener("pointerup", () => {
       this.panning = false;
       this.emit("pan.end");
+    });
+
+    // (mouse) wheel listener
+    let wheelTimeout = null;
+
+    target.addEventListener("wheel", () => {
+      this.wheelDelta = Math.sign(event.deltaY);
+      if (wheelTimeout === null) {
+        this.emit("wheel.start");
+      }
+      this.emit("wheel.move");
+      clearTimeout(wheelTimeout);
+      wheelTimeout = setTimeout(() => {
+        wheelTimeout = null;
+        this.emit("wheel.end");
+      }, 120);
     });
   }
 

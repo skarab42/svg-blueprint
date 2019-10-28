61b58af08dc27b9dc7ab5363ba33c4803809608e
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-10-28 11:44:26 +0100

    add pan/tap events

--- src/Pointer.js
+++ src/Pointer.js
@@ -1,206 +1,44 @@
 import Point from "./Point";
-import { addEvent, setStyle } from "./dom";
-
-function pointerFactory({ event, ...args }) {
-  return {
-    position: new Point(),
-    movement: new Point(),
-    distance: null,
-    midpoint: null,
-    scale: 1,
-    wheel: 0,
-    ...args,
-    event
-  };
-}
-
-function trackPointer(self, event) {
-  // calculate current position relative to the target element
-  const offsets = self.target.getBoundingClientRect();
-  const position = new Point(
-    event.pageX - offsets.left,
-    event.pageY - offsets.top
-  );
-
-  // get the old pointer object
-  const oldPointer = self.pointers.get(event.pointerId);
-
-  // calculate the movements
-  const movement = oldPointer ? position.sub(oldPointer.position) : new Point();
-
-  // set the new pointer
-  self.pointers.set(
-    event.pointerId,
-    pointerFactory({ event, position, movement })
-  );
-}
-
-function onPinch(self, id1, id2, distance = null) {
-  const p1 = self.pointers.get(id1);
-  const p2 = self.pointers.get(id2);
-  p1.distance = p2.distance = p1.position.distance(p2.position);
-  p1.midpoint = p2.midpoint = p1.position.midpoint(p2.position);
-  distance = distance === null ? p2.distance : distance;
-  p1.scale = p2.scale = p1.distance / distance;
-  return { p1, p2 };
-}
-
-function deletePointer(self, event) {
-  self.pointers.delete(event.pointerId);
-}
-
-function emit(self, name, event) {
-  // get the pointer data
-  const pointer = self.pointers.get(event.pointerId);
-
-  // search for callback
-  self.callbacks.forEach(callback => {
-    if (callback.name === "*") {
-      callback.func({ name, pointer });
-    } else if (callback.name === name) {
-      callback.func({ name, pointer });
-    }
-  });
-}
 
 /**
- * Poiter class.
+ * Pointer class.
  */
 class Pointer {
   /**
    * Pointer class constructor.
-   *
-   * @param {Element} element
    */
-  constructor(target) {
-    /** @type {Element} Target element. */
-    this.target = target;
-
-    /** @type {Map} Pointers collection. */
-    this.pointers = new Map();
-
-    /** @type {null|PointerEvent} Panning event. */
-    this.panEvent = null;
-
-    /** @type {null|array<PointerEvent>} Pinch events. */
-    this.pinchEvents = null;
-
-    /** @type {array} Callbacks list. */
-    this.callbacks = [];
-
-    // disable browser touch events on the target element
-    setStyle(target, "touch-action", "none");
-
-    // TRACK POINTERS
-    addEvent(
-      target,
-      "pointerdown pointermove pointerup pointerleave pointercancel",
-      event => {
-        trackPointer(this, event);
-      }
-    );
-
-    // POINTER PAN
-    addEvent(target, "pointerdown", event => {
-      if (this.panEvent || this.pinchEvents) {
-        return;
-      }
-      this.panEvent = event;
-      emit(this, "pan.start", event);
-    });
-
-    addEvent(target, "pointermove", event => {
-      if (this.panEvent && this.panEvent.pointerId === event.pointerId) {
-        emit(this, "pan.move", event);
-      }
-    });
-
-    addEvent(target, "pointerup pointerleave pointercancel", event => {
-      if (this.panEvent && this.panEvent.pointerId === event.pointerId) {
-        this.panEvent = false;
-        emit(this, "pan.end", event);
-      }
-    });
-
-    // PINCH ZOOM
-    addEvent(target, "pointerdown", event => {
-      if (!this.panEvent || this.pinchEvents) {
-        return;
-      }
-      if (this.panEvent.pointerId !== event.pointerId) {
-        const points = onPinch(this, this.panEvent.pointerId, event.pointerId);
-        this.pinchEvents = [this.panEvent, event, points.p1.distance];
-        this.panEvent = false;
-        emit(this, "pan.end", event);
-        emit(this, "pinch.start", event);
-      }
-    });
-
-    addEvent(target, "pointermove pointerup", event => {
-      if (!this.pinchEvents) {
-        return;
-      }
-
-      const ids = [
-        this.pinchEvents[0].pointerId,
-        this.pinchEvents[1].pointerId
-      ];
-
-      if (!ids.includes(event.pointerId)) {
-        return;
-      }
-
-      onPinch(this, ids[0], ids[1], this.pinchEvents[2]);
-      emit(this, "pinch.move", event);
-    });
-
-    addEvent(target, "pointerup", event => {
-      if (this.pinchEvents) {
-        this.pinchEvents = false;
-        emit(this, "pinch.end", event);
-      }
-    });
-
-    // MOUSE WHEEL
-    let wheelTimeout = null;
-
-    addEvent(target, "wheel", event => {
-      event.pointerId = "wheel";
-      trackPointer(this, event);
-
-      const pointer = this.pointers.get(event.pointerId);
-      pointer.wheel = event.deltaY > 0 ? 1 : -1;
-
-      if (wheelTimeout === null) {
-        emit(this, "wheel.start", event);
-      }
-
-      emit(this, "wheel.move", event);
-      clearTimeout(wheelTimeout);
-
-      wheelTimeout = setTimeout(() => {
-        wheelTimeout = null;
-        emit(this, "wheel.end", event);
-      }, 120);
-    });
-
-    // CLEANNING
-    addEvent(target, "pointerup pointerleave pointercancel", event => {
-      deletePointer(this, event);
-    });
+  constructor(pointerEvent) {
+    this.event = pointerEvent;
+    this.id = pointerEvent.pointerId;
+    this.position = new Point();
+    this.movement = new Point();
+    this.distance = 0;
+    this.down = false;
+    this.downTime = 0;
+    this.upTime = 0;
+    this.tapCount = 0;
+    this.tapDuration = 0;
+    this.panning = false;
+    this.panDistance = 0;
+    this.panStartPosition = 0;
+    this.panOffsets = new Point();
   }
 
-  /**
-   * Register event callback.
-   *
-   * @param {string}   name
-   * @param {function} func
-   */
-  on(name, func) {
-    const names = name.trim().split(/[\s,]+/);
-    names.forEach(name => {
-      this.callbacks.push({ name, func });
-    });
+  clone() {
+    const clone = {};
+    const keys = Object.keys(this);
+
+    for (let key of keys) {
+      let value = this[key];
+
+      if (value && value instanceof Point) {
+        value = value.clone();
+      }
+
+      clone[key] = value;
+    }
+
+    return clone;
   }
 }
 
---
+++ src/Pointers.js
@@ -0,0 +1,273 @@
+import { setStyle, addEvent } from "./dom";
+import Pointer from "./Pointer";
+import Point from "./Point";
+
+/**
+ * Defaults settings.
+ *
+ * @type {object}
+ */
+const settings = {
+  panThreshold: 10, // px
+  tapThreshold: 200, // ms
+  tapTimeout: 500 // ms
+};
+
+/**
+ * Pointers event types list.
+ *
+ * @type {array}
+ */
+const eventTypes = [
+  "pointerover",
+  "pointerenter",
+  "pointerdown",
+  "pointermove",
+  "pointerup",
+  "pointercancel",
+  "pointerout",
+  "pointerleave",
+  "gotpointercapture",
+  "lostpointercapture"
+];
+
+/**
+ * Pointers class.
+ */
+class Pointers {
+  /**
+   * Pointers class constructor.
+   *
+   * @param {Element} element
+   */
+  constructor(target, options = {}) {
+    /** @type {Element} Target element. */
+    this.target = target;
+
+    /** @type {object} Current settings. */
+    this.settings = { ...settings, ...options };
+
+    /** @type {array} Callbacks list. */
+    this.callbacks = [];
+
+    /** @type {Map} Pointerss collection. */
+    this.pointers = new Map();
+
+    /** @type {int} Tap counter. */
+    this.tapCount = 0;
+
+    /** @type {int} Tap timeout ID. */
+    this.tapTimeout = null;
+
+    // disable browser touch events on the target element
+    setStyle(target, "touch-action", "none");
+
+    // track all pointers events
+    addEvent(target, eventTypes.join(","), pointerEvent => {
+      this.trackPointer(pointerEvent);
+    });
+
+    // remove pointer on leave/cancel/lostcapture
+    addEvent(
+      target,
+      "pointerleave pointercancel lostpointercapture",
+      pointerEvent => {
+        this.deletePointer(pointerEvent.pointerId);
+      }
+    );
+  }
+
+  hasPointer(id) {
+    return this.pointers.has(id);
+  }
+
+  getPointer(id) {
+    return this.pointers.get(id);
+  }
+
+  setPointer(pointer) {
+    this.pointers.set(pointer.id, pointer);
+  }
+
+  deletePointer(id) {
+    if (this.pointers.has(id)) {
+      this.pointers.delete(id);
+    }
+  }
+
+  /**
+   * Return the relative position of the pointer from the target element.
+   *
+   * @param {PointerEvent} pointerEvent
+   *
+   * @return {Point}
+   */
+  getPosition(pointerEvent) {
+    const offsets = this.target.getBoundingClientRect();
+    return new Point(
+      pointerEvent.pageX - offsets.left,
+      pointerEvent.pageY - offsets.top
+    );
+  }
+
+  /**
+   * Tap recognizer.
+   *
+   * @param {string}  eventType
+   * @param {Pointer} pointer
+   */
+  tapRecognizer(eventType, pointer) {
+    if (eventType !== "up") {
+      return;
+    }
+
+    // calculate duration between up and down event
+    const tapDuration = pointer.upTime - pointer.downTime;
+
+    if (tapDuration > this.settings.tapThreshold) {
+      return; // time out
+    }
+
+    // clear tap timeout if any
+    this.tapTimeout && clearTimeout(this.tapTimeout);
+
+    // increment tap counter
+    pointer.tapCount = ++this.tapCount;
+    pointer.tapDuration = tapDuration;
+
+    // emit tap start event
+    if (this.tapCount === 1) {
+      this.emit("tap.start", pointer.clone());
+    }
+
+    // emit tap event...
+    this.emit("tap", pointer.clone());
+
+    // watch for tap end event
+    this.tapTimeout = setTimeout(() => {
+      this.emit("tap.end", pointer.clone());
+      this.tapCount = 0;
+    }, this.settings.tapTimeout);
+  }
+
+  /**
+   * Pan recognizer.
+   *
+   * @param {string}  eventType
+   * @param {Pointer} pointer
+   */
+  panRecognizer(eventType, pointer) {
+    if (eventType === "move") {
+      if (pointer.panStartPosition) {
+        pointer.panOffsets = pointer.position.sub(pointer.panStartPosition);
+        pointer.panDistance = pointer.panStartPosition.distance(
+          pointer.position
+        );
+        if (pointer.panning) {
+          this.emit("pan.move", pointer.clone());
+        } else if (pointer.panDistance > this.settings.panThreshold) {
+          pointer.panning = true;
+          this.emit("pan.start", pointer.clone());
+        }
+      }
+      return;
+    }
+
+    if (eventType === "down") {
+      if (!pointer.panStartPosition) {
+        pointer.panStartPosition = pointer.position.clone();
+      }
+      return;
+    }
+
+    if (["up", "leave", "cancel", "lostcapture"].includes(eventType)) {
+      if (pointer.panStartPosition) {
+        if (pointer.panning) {
+          this.emit("pan.end", pointer.clone());
+        }
+        pointer.panOffsets = new Point();
+        pointer.panStartPosition = 0;
+        pointer.panDistance = 0;
+        pointer.panning = false;
+      }
+      return;
+    }
+  }
+
+  /**
+   * Track pointer event.
+   *
+   * @param {PointersEvent} pointerEvent
+   */
+  trackPointer(pointerEvent) {
+    // event type
+    const eventType = pointerEvent.type.replace("pointer", "");
+
+    // get the pointer
+    let pointer = this.getPointer(pointerEvent.pointerId);
+
+    // get current position
+    const position = this.getPosition(pointerEvent);
+
+    // add new pointer if not already set
+    if (!pointer) {
+      pointer = new Pointer(pointerEvent);
+      pointer.position = position;
+      this.setPointer(pointer);
+    }
+
+    // update pointer
+    if (eventType === "move") {
+      pointer.distance = pointer.position.distance(position);
+      pointer.movement = position.sub(pointer.position);
+      pointer.position = position;
+    } else if (eventType === "down") {
+      pointer.downTime = Date.now();
+      pointer.down = true;
+    } else if (eventType === "up") {
+      pointer.upTime = Date.now();
+      pointer.down = false;
+    }
+
+    // emit event
+    this.emit(eventType, pointer.clone());
+
+    // recognizers
+    this.tapRecognizer(eventType, pointer);
+    this.panRecognizer(eventType, pointer);
+  }
+
+  /**
+   * Register event callback.
+   *
+   * @param {array|string} type
+   * @param {function}     func
+   */
+  on(type, func) {
+    let types = type;
+
+    if (typeof types === "string") {
+      types = types.trim().split(/[\s,]+/);
+    }
+
+    types.forEach(type => {
+      this.callbacks.push({ type, func });
+    });
+  }
+
+  /**
+   * Emit an event.
+   *
+   * @param {string} type
+   * @param {object} [data={}]
+   */
+  emit(type, data = {}) {
+    this.callbacks.forEach(callback => {
+      if (callback.type === type || callback.type === "*") {
+        callback.func({ type, data });
+      }
+    });
+  }
+}
+
+export default Pointers;

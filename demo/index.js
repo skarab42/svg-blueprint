36a75d9890d50f3aaf429e25ab6385fce8cd1af7
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-11-08 08:32:54 +0100

    Update index.js

--- demo/index.js
+++ demo/index.js
@@ -7,7 +7,7 @@ const blueprint = new sbp.Blueprint({
 });
 
 // add simple shape
-blueprint.append('rect', { x: -50, y: -50, width: 100, height: 100 });
+blueprint.append('rect', { x: -40, y: -40, width: 80, height: 80 });
 
 // append maker.js model
 const smile = new SmileModel();
